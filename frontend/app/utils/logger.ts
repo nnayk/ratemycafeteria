4cd891eeae86f21f1c8d5aa0b4782e46be9400d9
Nakul Nayak <nakul.indus@gmail.com>
2025-04-02 14:31:05 +0530

    Remove comments

--- frontend/app/utils/logger.ts
+++ frontend/app/utils/logger.ts
@@ -17,52 +17,13 @@
 
 const BETTERSTACK_URL = "https://s1229187.eu-nbg-2.betterstackdata.com/";
 
-//   //   console.log(`[${level.toUpperCase()}]`, message, meta); // Local debugging
-//   //   return;
-//   // }
-
-//   await fetch(BETTERSTACK_URL, {
-//     method: "POST",
-//     headers: { 
-//         "Content-Type": "application/json",
-//         "Authorization": `Bearer 1YneWBtAipW6pVmDKj84gehu`
-//     },
-//     body: JSON.stringify({
-//       level,
-//       message,
-//       meta,
-//       timestamp: new Date().toISOString(),
-//       app: "RateMyCafeteria",
-//     }),
-//   });
-// };
-
-// export const logger = {
-//   info: (msg: string, meta = {}) => sendLog("info", msg, meta),
-//   warn: (msg: string, meta = {}) => sendLog("warn", msg, meta),
-//   error: (msg: string, meta = {}) => sendLog("error", msg, meta),
-// };
-
-// export const log = (...args: unknown[]) => {
-//       logger.info(args.map(arg => String(arg)).join(" ")); // Pass as an array instead of spreading
-// };
-
-// const sendLog = async (level: "info" | "warn" | "error", message: string, meta: object = {}) => {
-//       await fetch("/api/log", {
-//               method: "POST",
-//                   headers: { "Content-Type": "application/json" },
-//                       body: JSON.stringify({ level, message, meta }),
-//                         });
-// };
-
-// export const log = async  (...args: unknown[]) => {
-//     await sendLog("info", args.map(arg => String(arg)).join(" ")); // Pass as an array instead of spreading
-// }; 
-
 const sendLog = async (level: "info" | "warn" | "error" | "debug", message: string, meta: object = {}) => {
-  await fetch("/api/log", {
+  await fetch(BETTERSTACK_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { 
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + process.env.NEXT_PUBLIC_BETTERSTACK_API_KEY
+    },
     body: JSON.stringify({ level, message, meta }),
   });
 };
