3e5f1db7ba5ba503728e4095b83e944a85be8fcc
Nakul Nayak <nakul.indus@gmail.com>
2025-08-23 15:51:47 -0700

    Add script to read csv and submit source reviews
    Need to fix it to admit reviews into a school sub collection. Right now
    it submits reviews into top level collection under /sourcedReviews

---
+++ backend/scripts/dbMigration/addSourcedReviews.py
@@ -0,0 +1,40 @@
+import csv
+import firebase_admin                                                           
+from firebase_admin import credentials, firestore                               
+# Initialize Firebase Admin SDK                                                 
+cred = credentials.Certificate("prod.json")  # Path to your service account JSON
+firebase_admin.initialize_app(cred)                                             
+                                                                                
+# Get Firestore database instance                                               
+db = firestore.client()                                              
+# from utils import db
+
+def add_reviews_from_csv(file_path):
+    """
+    Reads a CSV file with web source, review text, and web link,
+    and adds the data to the 'sourcedReviews' collection in Firebase.
+    """
+    # db = init_firebase()
+    sourced_reviews_ref = db.collection('sourcedReviews')
+
+    with open(file_path, mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            try:
+#,Source / Platform,Full Review Text,Source Link
+                review_data = {
+                    'source': row['Source / Platform'],
+                    'reviewText': row['Full Review Text'],
+                    'link': row['Source Link']
+                }
+                sourced_reviews_ref.add(review_data)
+                print(f"Successfully added review from {row['Source / Platform']}")
+            except KeyError as e:
+                print(f"Skipping row due to missing column: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+if __name__ == '__main__':
+    # Replace 'path/to/your/csv_file.csv' with the actual path to your CSV file.
+    fp = "/Users/nnayak/Documents/other/proj/rmc/ratemycafeteria/backend/scripts/scraped-reviews/vista_grande_reviews.csv"
+    add_reviews_from_csv(fp)
